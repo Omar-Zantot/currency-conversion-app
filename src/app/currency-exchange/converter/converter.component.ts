79233f39f51534f44d38f07268a52f62c6053368
salma <sakrsalma42@gmail.com>
2023-08-24 15:07:03 +0300

    src

--- src/app/currency-exchange/converter/converter.component.ts
+++ src/app/currency-exchange/converter/converter.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-converter',
   templateUrl: './converter.component.html',
   styleUrls: ['./converter.component.scss']
 })
 export class ConverterComponent {
-
+  value = 'Clear me';
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Egypt' },
+    { value: 'pizza-1', viewValue: 'United States' },
+    { value: 'tacos-2', viewValue: 'Australia' },
+  ];
 }
--- src/app/currency-exchange/currency-exchange.module.ts
+++ src/app/currency-exchange/currency-exchange.module.ts
@@ -5,17 +5,32 @@ import { CurrencyExchangeRoutingModule } from './currency-exchange-routing.modul
 import { CurrencyExchangeComponent } from './currency-exchange.component';
 import { ConverterComponent } from './converter/converter.component';
 import { CompareComponent } from './compare/compare.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+// import { CascadeSelectModule } from 'primeng/cascadeselect';
+
 
 
 @NgModule({
   declarations: [
     CurrencyExchangeComponent,
     ConverterComponent,
-    CompareComponent
+    CompareComponent,
+
   ],
   imports: [
     CommonModule,
-    CurrencyExchangeRoutingModule
+    CurrencyExchangeRoutingModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
   ]
 })
 export class CurrencyExchangeModule { }
