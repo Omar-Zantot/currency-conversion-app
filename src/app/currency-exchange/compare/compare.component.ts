d3b06ccf4821981928c86ad608fba8cce6ce6113
fareeda <faridaallam2002@gmail.com>
2023-08-24 22:46:59 +0300

    compare design

--- src/app/currency-exchange/compare/compare.component.ts
+++ src/app/currency-exchange/compare/compare.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+interface Country {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-compare',
   templateUrl: './compare.component.html',
   styleUrls: ['./compare.component.scss']
 })
 export class CompareComponent {
-
+  value = 'Clear me';
+  countries: Country[] = [
+    { value: 'EGY-0', viewValue: 'Egypt' },
+    { value: 'USA-1', viewValue: 'United States' },
+    { value: 'AUS-2', viewValue: 'Australia' },
+  ];
 }
